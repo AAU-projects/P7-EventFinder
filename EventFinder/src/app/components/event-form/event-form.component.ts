4660a091c5c4444953b973946d6c1837502e50d0
Alechj <alec@hotmail.dk>
2019-10-21 15:15:50 +0200

    Functional Event add form

--- EventFinder/src/app/components/event-form/event-form.component.ts
+++ EventFinder/src/app/components/event-form/event-form.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { RegExValidator } from 'src/app/directives/regEx.directive';
+import { EventService } from 'src/app/services/event.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Dresscode, Genre, Atmosphere } from 'src/app/models/event.model';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-event-form',
@@ -9,18 +13,28 @@ import { RegExValidator } from 'src/app/directives/regEx.directive';
 })
 export class EventFormComponent implements OnInit {
 
+  get Dresscode() { return Dresscode; }
+  get Genre() { return Genre; }
+  get Atmosphere() { return Atmosphere; }
+
   eventForm: FormGroup;
-  genreList: string[];
-  atmosList: string[];
-  dressChoice: string;
+  genreList: Genre[];
+  atmosList: Atmosphere[];
+  dressChoice: Dresscode;
+  isFree = false;
+  bannerFilePath = '';
+  bannerFileName: string;
+  bannerEvent: any;
 
   constructor(
     private fb: FormBuilder,
+    private eventService: EventService,
+    private authService: AuthService,
+    private storageService: StorageService,
   ) {
     this.createForm();
     this.genreList = [];
     this.atmosList = [];
-    this.dressChoice = '';
    }
 
   ngOnInit() {
@@ -28,14 +42,18 @@ export class EventFormComponent implements OnInit {
 
   createForm() {
     this.eventForm = this.fb.group({
+      organizerId: this.authService.user.uid,
+      startDate: ['', [Validators.required]],
+      endDate: ['', [Validators.required]],
+      startTime: ['', [Validators.required]],
+      endTime: ['', [Validators.required]],
       title: ['', [Validators.required]],
       description: ['', [Validators.required]],
-      banner: ['', [Validators.required]],
-      genre: ['', [Validators.required]],
-      genreCustom: ['', [Validators.required]],
-      atmosphere: ['', [Validators.required]],
-      atmosphereCustom: ['', [Validators.required]],
-      dresscode: ['', [Validators.required]],
+      banner: ['', []],
+      genre: ['', []],
+      atmosphere: ['', []],
+      atmosphereCustom: ['', []],
+      dresscode: ['', []],
       address: ['', [Validators.required]],
       zip: ['', [Validators.required, RegExValidator(/^[0-9]{4}$/i)]],
       city: ['', [Validators.required, Validators.minLength(3), RegExValidator(/[a-z, ,A-Z,ÆæØøÅå]*/i)]],
@@ -46,28 +64,58 @@ export class EventFormComponent implements OnInit {
   }
 
   createEvent(value) {
+    if (this.isFree) {
+      value.price = 0;
+    }
 
+    value.atmosphere = this.atmosList;
+    value.genre = this.genreList;
+    value.banner = this.bannerFilePath;
+    value.dresscode = this.dressChoice;
+
+    value.startDate = this.convertDateTime(value.startDate, value.startTime);
+    value.endDate = this.convertDateTime(value.endDate, value.endTime);
+
+    this.UploadBanner();
+    value.banner = this.bannerFilePath;
+    this.eventService.createEvent(value);
   }
 
-  onGenreClick(text: string) {
-    if (this.genreList.includes(text)) {
-      this.genreList.splice(this.getListIndex(text, this.genreList), 1);
+  convertDateTime(inputDate, inputTime){
+    let date = new Date(inputDate);
+    let timeString = inputTime.toString();
+    date.setHours(timeString.split(':')[0]);
+    date.setMinutes(timeString.split(':')[1]);
+    return date.toISOString();
+  }
+
+  onFileInputChanged(event) {
+    this.bannerFileName = event.target.files.item(0).name;
+    this.bannerEvent = event;
+  }
+
+  UploadBanner() {
+    this.bannerFilePath = this.storageService.uploadEventBanner(this.bannerEvent, this.bannerFileName);
+  }
+
+  onGenreClick(genre: Genre) {
+    if (this.genreList.includes(genre)) {
+      this.genreList.splice(this.getListIndex(genre, this.genreList), 1);
     } else {
-      this.genreList.push(text);
+      this.genreList.push(genre);
     }
   }
 
-  onAtmosClick(text: string) {
-    if (this.atmosList.includes(text)) {
-      this.atmosList.splice(this.getListIndex(text, this.atmosList), 1);
+  onAtmosClick(atmos: Atmosphere) {
+    if (this.atmosList.includes(atmos)) {
+      this.atmosList.splice(this.getListIndex(atmos, this.atmosList), 1);
     } else {
-      this.atmosList.push(text);
+      this.atmosList.push(atmos);
     }
   }
 
-  onDressClick(text: string) {
-    this.dressChoice = text;
-    console.log(this.dressChoice);
+  onDressClick(dresscode: Dresscode) {
+    this.dressChoice = dresscode;
   }
 
   getListIndex(text: string, list: string[]): number {
