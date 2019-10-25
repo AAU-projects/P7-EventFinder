de3cab73bd0e3b75e4516f255a712a7886be7b56
Alechj <Alechj@users.noreply.github.com>
2019-10-25 11:10:17 +0200

    Event form (#47)
    * Fix formatting for release
    
    * Added Event-Form component.
    
    * Setup the form.
    
    * Add more design and Functionality
    
    * Functional Event add form
    
    * Eventform complete. (with submenus!)
    
    * RichText support (first try)
    
    * EventForm done

--- EventFinder/src/app/components/components.module.ts
+++ EventFinder/src/app/components/components.module.ts
@@ -4,23 +4,50 @@ import { TestComponent } from './test/test.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { TwoStateButtonComponent } from './two-state-button/two-state-button.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserInfoComponent } from './user-info/user-info.component';
 import { NavbarUserDropdownComponent } from './navbar-user-dropdown/navbar-user-dropdown.component';
 import { OrganizerInfoComponent } from './organizer-info/organizer-info.component';
+import { EventFormComponent } from './event-form/event-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { QuillModule } from 'ngx-quill';
 
 /* How to add a new component:
     ng g c components/[name] --export
 */
 
 @NgModule({
-  declarations: [TestComponent, HeaderComponent, TwoStateButtonComponent, UserInfoComponent, NavbarUserDropdownComponent, OrganizerInfoComponent],
+  declarations: [
+    TestComponent,
+    HeaderComponent,
+    TwoStateButtonComponent,
+    UserInfoComponent,
+    NavbarUserDropdownComponent,
+    OrganizerInfoComponent,
+    EventFormComponent],
   imports: [
     CommonModule,
     RouterModule,
     FormsModule,
-  ],
-  exports: [TestComponent, HeaderComponent, TwoStateButtonComponent, UserInfoComponent, NavbarUserDropdownComponent, OrganizerInfoComponent],
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    QuillModule.forRoot({
+      modules: {
+        syntax: false,
+        toolbar: [
+          ['bold', 'italic', 'underline'],
+          [{ 'header': 1 }, { 'header': 2 }]],
+      },
+      placeholder: 'Write description here...'
+    })],
+  exports: [
+    TestComponent,
+    HeaderComponent,
+    TwoStateButtonComponent,
+    UserInfoComponent,
+    NavbarUserDropdownComponent,
+    OrganizerInfoComponent,
+    EventFormComponent],
 })
 export class ComponentsModule {}
 
