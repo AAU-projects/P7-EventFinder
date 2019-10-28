a176174a48fa2cc95431c1d3dc7907ee25e0bb2a
Rasmus Egholm Nielsen <th3nielsen@hotmail.com>
2019-10-28 11:17:19 +0100

    Represent data properly

--- EventFinder/src/app/components/user-info/user-info.component.ts
+++ EventFinder/src/app/components/user-info/user-info.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from 'src/app/models/account.model';
+import { DatePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
+  providers: [DatePipe, TitleCasePipe],
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss']
@@ -9,9 +11,16 @@ import { User } from 'src/app/models/account.model';
 export class UserInfoComponent implements OnInit {
   @Input() user: User;
 
-  constructor() { }
+  constructor(public datepipe: DatePipe, private titlecasePipe: TitleCasePipe ) { }
 
   ngOnInit() {
   }
 
+  displaySex() {
+    return this.titlecasePipe.transform(this.user.sex);
+  }
+
+  displayBirthdate() {
+    return this.datepipe.transform(this.user.birthday, 'dd/MM/yyyy');
+  }
 }
