fd3eb2d692be158b7a1d3e7281d8606576a7122d
LukeRH <lukasronsholt@gmail.com>
2019-10-28 14:54:29 +0100

    LoggedinGuard
    Co-authored-by: Lasse Lundbo <llun16@student.aau.dk>

--- EventFinder/src/app/pages/pages-routing.module.ts
+++ EventFinder/src/app/pages/pages-routing.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 import { FirebaseTestComponent } from './firebase-test/firebase-test.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { UserComponent } from './user/user.component';
 import { RegisterComponent } from './register/register.component';
-import { AuthGuard } from '../guards/auth.guard';
 import { EventsComponent } from './events/events.component';
 import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
 import { OrganizerComponent } from './organizer/organizer.component';
-
+import { LoggedInGuard } from '../guards/logged-in.guard';
+import { NotLoggedInGuard } from '../guards/not-logged-in.guard';
 
 // All paths starts from root (/)
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'register', component: RegisterComponent},
+  {path: 'register', component: RegisterComponent, canActivate: [NotLoggedInGuard]},
   {path: 'firebase', component: FirebaseTestComponent},
-  {path: 'user', component: UserComponent},
-  {path: 'organizer', component: OrganizerComponent},
+  {path: 'user', component: UserComponent, canActivate: [LoggedInGuard]},
+  {path: 'organizer', component: OrganizerComponent, canActivate: [LoggedInGuard]},
   {path: 'events', component: EventsComponent},
   {path: 'forgotpassword', component: ForgotpasswordComponent},
 
