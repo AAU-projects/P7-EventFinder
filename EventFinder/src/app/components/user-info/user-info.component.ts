1c660293a7e9e1f9d41a21ce932e6b8429c721ae
Rasmus Egholm Nielsen <th3nielsen@hotmail.com>
2019-10-30 14:10:10 +0100

    Revert "Merge branch 'master' into #57-Give-user-feedback-when-they-logout"
    This reverts commit 3ff7c33b2933d2b5184b7641e38756911e80bfdd, reversing
    changes made to 00afcd5bad39cc710e06ca09ec952e41db9579f5.

--- EventFinder/src/app/components/user-info/user-info.component.ts
+++ EventFinder/src/app/components/user-info/user-info.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from 'src/app/models/account.model';
-import { DatePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
-  providers: [DatePipe, TitleCasePipe],
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss']
@@ -11,16 +9,9 @@ import { DatePipe, TitleCasePipe } from '@angular/common';
 export class UserInfoComponent implements OnInit {
   @Input() user: User;
 
-  constructor(public datepipe: DatePipe, private titlecasePipe: TitleCasePipe ) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  displaySex() {
-    return this.titlecasePipe.transform(this.user.sex);
-  }
-
-  displayBirthdate() {
-    return this.datepipe.transform(this.user.birthday, 'dd/MM/yyyy');
-  }
 }
