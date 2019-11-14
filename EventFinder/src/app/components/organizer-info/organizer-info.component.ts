b611a606842c7e761d19f618e7f38d06c7f18c4d
HaZz <magnustboisen@gmail.com>
2019-11-14 11:00:49 +0100

    organization events

--- EventFinder/src/app/components/organizer-info/organizer-info.component.ts
+++ EventFinder/src/app/components/organizer-info/organizer-info.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Organizer } from 'src/app/models/account.model';
+import { Event } from 'src/app/models/event.model';
+import { EventService } from 'src/app/services/event.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-organizer-info',
@@ -8,10 +11,19 @@ import { Organizer } from 'src/app/models/account.model';
 })
 export class OrganizerInfoComponent implements OnInit {
   @Input() user: Organizer;
+  orgEventList: Event[] = [];
 
-  constructor() { }
+  constructor(private eventService: EventService) {}
 
   ngOnInit() {
-  }
+    this.eventService.getEvents(100).subscribe(result => {
+      for (const collection of result) {
+        const event = collection.payload.doc.data() as Event;
 
+        if (event.organizerId === this.user.uid) {
+          this.orgEventList.push(event);
+        }
+      }
+    });
+  }
 }
